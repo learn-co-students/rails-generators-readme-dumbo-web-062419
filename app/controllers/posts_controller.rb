d27d71d84c37748037a0d9bc75f7105792baec8d
lgm527 <laurellmccaffrey@gmail.com>
2019-07-21 15:38:50 -0400

    generators

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,6 @@ class PostsController < ApplicationController
   	@posts = Post.all
   end
 
-  def show
-  	@post = Post.find(params[:id])
-  end
-
   def new
   	@post = Post.new
   end
@@ -17,13 +13,24 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
-    redirect_to post_path(@post)
-  end
-
   def edit
     @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
 end
